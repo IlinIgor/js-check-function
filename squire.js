669e5ae93a961c9038b6cd8009f5fa356884b803
Игорь Ильин <igorilin@MacBook-Pro-Igor.local>
2018-07-06 14:33:29 +0300

    Add square function

---
+++ squire.js
@@ -0,0 +1,10 @@
+// DO not use any kind of iterations (for-loops, etc)
+function square(array) {
+  return (function squareItem (newArray) {
+    if (newArray.length !== array.length) {
+      newArray.push(array[newArray.length] * array[newArray.length])
+      squareItem(newArray)
+    }
+    return newArray
+  })([])
+}
